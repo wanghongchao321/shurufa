2c9fef115405a97511633dd363343ea34edcd3e8
wanghongchao321 <wanghongchao321@gmail.com>
2026-08-26 08:20:20 +0000

    fix: use public RowScope weight modifier

--- app/src/main/java/com/kingzcheung/xime/ui/keyboard/AiModeBar.kt
+++ app/src/main/java/com/kingzcheung/xime/ui/keyboard/AiModeBar.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
